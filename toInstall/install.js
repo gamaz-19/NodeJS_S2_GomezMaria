c0139b3d25f28a9b12e9a10686f4aad55d61f545
Maria <maga1731@gmail.com>
2025-11-07 12:29:51 -0500

    "feat: :sparkles: Uploading missing files"

---
+++ toInstall/install.js
@@ -0,0 +1,14 @@
+#Comandos para poner en el .gitignore
+**/node_modules/
+**/.env
+
+#Paquetes necesarios para correr los programas
+
+#Instalacion 
+
+nvm -v #comprobar la version del npm
+nvm install --lts #instalacion de la ultima version 
+
+#Creacion de proyecto
+npm init -y #init crea el proeycto y el -y se asegura de aprobar y dar aprobacion a todas las aprobaciones
+
